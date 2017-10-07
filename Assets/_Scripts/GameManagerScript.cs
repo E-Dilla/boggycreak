b526b8c951c0ed02afcfa60c7d25266b70f6db50
E-DILLA-PC\E-Dilla <eenterprisestudios@gmail.com>
2017-10-07 02:03:45 -0500

    game Manager
    for Boggy to check time

---
+++ Assets/_Scripts/GameManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    public bool isDayTime;
+    public bool isNightTime;
+
+    public float clockTime;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        clockTime += Time.deltaTime;
+        
+        if (clockTime >= 360)
+        {
+            clockTime = 0;
+        }
+
+        if (clockTime <= 180f)
+        {
+            isDayTime = true;
+        }
+        else
+        {
+            isDayTime = false;
+        }
+
+	}
+}
